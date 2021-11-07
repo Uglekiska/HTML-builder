d100a0fb5821a4a33812a59a56b1115f9e45e562
Uglekiska <uglekiska@yandex.by>
2021-11-07 22:51:19 +0300

    fix: complete copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const path = require('path');
+const fs = require('fs');
+const testFolder = './04-copy-directory/files/';
+var filesO = fs.readdirSync(testFolder, {
+	withFileTypes: true
+}).filter(item => !item.isDirectory()).map(item => item.name);
 
+function copyFolder() {
+
+	fs.stat('./04-copy-directory/files-copy', function (err) {
+		if (!err) {
+			fs.readdir('./04-copy-directory/files-copy', (err, files) => {
+				if (err) throw err;
+				for (const fileN of files) {
+					fs.unlink(path.join('./04-copy-directory/files-copy', fileN), err => {
+						if (err) throw err;
+					});
+					for (let i = 0; i < filesO.length; i++) {
+						fs.createReadStream(`./04-copy-directory/files/${filesO[i]}`).pipe(fs.createWriteStream(`./04-copy-directory/files-copy/${filesO[i]}`));
+					};
+				}
+			});
+			
+
+		} else if (err.code === 'ENOENT') {
+			fs.mkdir('./04-copy-directory/files-copy', () => {});
+			for (let i = 0; i < filesO.length; i++) {
+				fs.createReadStream(`./04-copy-directory/files/${filesO[i]}`).pipe(fs.createWriteStream(`./04-copy-directory/files-copy/${filesO[i]}`));
+			}
+		}
+	});
+}
+
+copyFolder();
